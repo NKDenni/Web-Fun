2997e65ab37641e14cfa787cfff5a50ba0c0d180
Nathan Denni <aubakasan303@gmail.com>
2021-05-24 14:53:24 -0700

    Playing with Objects first commit

---
+++ JavaScript/Playing_With_Objects/PlayingWithObjects.js
@@ -0,0 +1,6 @@
+var users = [{ name: "Michael", age: 37 }, { name: "John", age: 30 }, { name: "David", age: 27 }];
+
+for (var i = 0; i < users.length; i++) {
+    var user = users[i];
+    console.log(user.name, "-", user.age);
+}
