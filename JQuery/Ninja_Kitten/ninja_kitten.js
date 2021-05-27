724f8d8741fc81d297be4ffbbb585f8b987cc3aa
Nathan Denni <aubakasan303@gmail.com>
2021-05-27 16:02:24 -0700

    fixed something in troopers. 1st commit ninja cat

---
+++ JQuery/Ninja_Kitten/ninja_kitten.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+
+    $('img').click(function () {
+        var image = $(this).attr('src');
+        var kitten = $(this).attr('data-alt-src'); 
+        $(this).attr('src', kitten);
+        $(this).attr('data-alt-src', image);
+    })
+})
